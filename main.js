633269430315e5e969a9e7b6d162e54ebe64e768
Yuriy Kolodiy <macroch@yandex.ru>
2018-05-04 17:55:30 +0300

    add projects block

--- main.js
+++ main.js
@@ -4,3 +4,13 @@ $(function () {
         slidesToShow: 8
     });
 });
+
+let item = document.querySelectorAll('.item');
+
+[...item].forEach(item => {
+    let percentValue = Math.round(+item.querySelector('.item__in').innerHTML / +item.querySelector('.item__need').innerHTML * 100) + '%';
+    // console.log(percentValue);
+    item.querySelector('.item__percent').innerHTML = percentValue;
+    item.querySelector('.item__bar').style.width = percentValue;
+});
+
