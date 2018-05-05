fec282db3d64e1bbea41d8468a7ede03a3be8f37
Yuriy Kolodiy <macroch@yandex.ru>
2018-05-05 13:46:50 +0300

    add adaptive

--- main.js
+++ main.js
@@ -1,7 +1,33 @@
 $(function () {
     $('.slider__items').slick({
         arrows: false,
-        slidesToShow: 8
+        slidesToShow: 8,
+        responsive: [
+            {
+                breakpoint: 1499,
+                settings: {
+                    slidesToShow: 6,
+                }
+            },
+            {
+                breakpoint: 1199,
+                settings: {
+                    slidesToShow: 4,
+                }
+            },
+            {
+                breakpoint: 767,
+                settings: {
+                    slidesToShow: 3,
+                }
+            },
+            {
+                breakpoint: 579,
+                settings: {
+                    slidesToShow: 1,
+                }
+            }
+        ]
     });
 });
 
@@ -12,5 +38,13 @@ let item = document.querySelectorAll('.item');
     // console.log(percentValue);
     item.querySelector('.item__percent').innerHTML = percentValue;
     item.querySelector('.item__bar').style.width = percentValue;
+});
+
+let burger = document.querySelector('.navMain__btn');
+let navList = document.querySelector('.navMain ul');
+
+burger.addEventListener('click', function () {
+    this.classList.toggle('open');
+    navList.classList.toggle('visible')
 });
 
